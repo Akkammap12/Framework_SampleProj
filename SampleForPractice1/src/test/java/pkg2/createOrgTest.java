4db3ac137e4de7971310c5158b68bff9c289461d
Akkamma-github <akkammapatil64@gmail.com>
2025-03-20 15:12:51 +0530

    added modifi

--- SampleForPractice1/src/test/java/pkg2/createOrgTest.java
+++ SampleForPractice1/src/test/java/pkg2/createOrgTest.java
@@ -6,6 +6,17 @@ public class createOrgTest {
 	@Test
 	public void createOrg() 
 	{
+		
+		String URL = System.getProperty("url");	
+		String BROWSER = System.getProperty("browser", "chrome");
+		String USERNAME = System.getProperty("username");
+		String PASSWORD = System.getProperty("password");
+		
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
+		
 	System.out.println("====Executed createOrgTest====");
 	
 	}
